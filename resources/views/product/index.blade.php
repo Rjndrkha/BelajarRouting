8719782a03165b15915fdd87ac5ca54391eeab8f
Rjndrkha <75615789+Rjndrkha@users.noreply.github.com>
2021-11-17 18:28:42 +0700

    first commit

---
+++ resources/views/product/index.blade.php
@@ -0,0 +1,20 @@
+<h1>List Products</h1>
+<h1>{{$judul}}</h1>
+
+<table>
+    <thead>
+        <tr>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Price</th>
+        </tr>
+    </thead>
+
+    <tbody>
+        <tr>
+            <td>Celana</td>
+            <td>Nyaman</td>
+            <td>Rp. 45.000</td>
+        </tr>
+    </tbody>
+</table>
